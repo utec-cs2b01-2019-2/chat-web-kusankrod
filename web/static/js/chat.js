5bab1075e51e4403ab51bcccf0cc22cb68d6fb2a
Kurt Rafael Osterling Dankers <kurt.osterling@utec.edu.pe>
2019-09-27 10:52:09 -0500

    Chat HTML

---
+++ web/static/js/chat.js
@@ -0,0 +1,22 @@
+
+
+function sendMessage(){
+    alert("Happy Birthday Bruno uwu");
+}
+
+function get_all_users(){
+    console.log("Voy a traer a todos los usuarios");
+
+    $.getJSON("/users", function(data){
+    var i =0;
+    $.each(data, function(){
+        user_to = data[i]["id"];
+        e = '<div class="alert" role ="alert" >';
+        e = e+"<div>"+data[i]["username"]+"</div>";
+        e = e+"</div>";
+        i = i+1
+        $("<div/>", {html:e}).appendTo("#users");
+    });
+    });
+};
+
